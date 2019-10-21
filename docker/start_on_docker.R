f454edac53fff479c5f83a590cbc34379a8d2037
Johan Elkink <jos.elkink@ucd.ie>
2019-10-21 19:31:25 +0100

    Attempt to switch to multiple instances of Shiny

--- docker/start_on_docker.R
+++ docker/start_on_docker.R
@@ -1,10 +1,28 @@
+getEnvInt <- function(name, default = 0) {
+  if (is.na(as.integer(Sys.getenv(name))))
+    default
+  else
+    as.integer(Sys.getenv(name))
+}
+
+getEnvStr <- function(name, default = "") {
+  if (Sys.getenv(name) == "")
+    default
+  else
+    Sys.getenv(name)
+}
+
+
+port <- getEnvInt("PORT", 80)
+host <- getEnvStr("HOST", "172.17.0.2")
+
 setwd('/srv/shiny-server/docker/')
 
 data <- rio::import('../docker/data/ESS_FPR.Rdata')
 
 devtools::install_github('jelkink/ggplotgui')
 
-options(shiny.port = 80)
-options(shiny.host = "172.17.0.2")
+options(shiny.port = port)
+options(shiny.host = host)
 
 ggplotgui::ggplot_shiny(data)

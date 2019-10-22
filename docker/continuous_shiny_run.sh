d72b6b6b59b55778eb9835b1642a75e416f73d83
Johan Elkink <jos.elkink@ucd.ie>
2019-10-22 17:27:21 +0100

    Added automatic detection of local IP

--- docker/continuous_shiny_run.sh
+++ docker/continuous_shiny_run.sh
@@ -6,6 +6,6 @@
 
 while true
 do
-    R -f /srv/shiny-server/docker/start_on_docker.R
+    HOST=`ip route show | grep src | sed "s/.*src //" | sed "s/ //"` R -f /srv/shiny-server/docker/start_on_docker.R
     sleep 1
 done
